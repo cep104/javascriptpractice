691b693f8193646d3b39dfcd4b6e3cc5f2b7340e
cep104 <candicelizabethpeters@gmail.com>
2020-12-12 23:25:36 -0600

    using functions inside of functions

---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    fruitProcessor()
+});
+
+function cutFruitPieces(fruit){
+    return fruit * 4
+}
+
+function fruitProcessor(apples, oranges){
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges. `
+    return juice
+}
+
