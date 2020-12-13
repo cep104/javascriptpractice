5afa10f8e33854427f9386a5ceed524b3ed87c3b
cep104 <candicelizabethpeters@gmail.com>
2020-12-12 23:58:08 -0600

    putting it all together practice using selectors and funcitons inside of functions

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+//calling a function inside of a function
+
 document.addEventListener('DOMContentLoaded', (event) => {
     fruitProcessor()
+    yearsUntilRetirement()
 });
 
 function cutFruitPieces(fruit){
@@ -9,7 +12,27 @@ function cutFruitPieces(fruit){
 function fruitProcessor(apples, oranges){
     const applePieces = cutFruitPieces(apples);
     const orangePieces = cutFruitPieces(oranges);
-    const juice = `Juice with ${apples} apples and ${oranges} oranges. `
+    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges. `
     return juice
+}
+
+const calcAge = function(birthYear){
+    return 2037 - birthYear
+}
+
+
+
+const yearsUntilRetirement = function(birthYear, firstName = 'person'){
+    const age = calcAge(birthYear)
+    const retirement = 65 - age; 
+    const body = document.querySelector("body")
+    const div = document.createElement("div")
+    body.appendChild(div)
+    if(retirement > 0){
+    div.innerHTML = `${firstName} retires in ${retirement} years`;
+    }else{
+        div.innerHTML = `${firstName} has already retired`;
+        //return -1 to show they have already retired 
+    }
 }
 
