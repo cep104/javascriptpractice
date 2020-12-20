f9065fe4d3d5279e9ed547d0d1ec8ffc0af192b1
cep104 <candicelizabethpeters@gmail.com>
2020-12-19 22:15:16 -0600

    while loop

--- loops.js
+++ loops.js
@@ -41,3 +41,53 @@ for(let i = 0; i < jonasArray.length; i++){
  console.log(jonasArray[i], typeof jonasArray[i])
 }
 ///as soon as it finds the first number break and terminate the loop
+
+console.log('-----BACKWARDS')
+for(let i = jonasArray.length - 1 ; i >= 0; i--){
+    ///for counting down you make the counter the length -1 so it starts at zero 
+    ///
+    console.log(i,jonasArray[i])
+}
+
+for(let exercise = 1; exercise <=3; exercise++){
+    console.log(`---------Starting exercise ${exercise}`)
+
+
+    for(let rep=1; rep <= 5; rep++){
+        console.log(`Excercise ${exercise} Lifting weight repetition ${rep}`)
+        //loop inside of a loop
+    }
+}
+//repeating the same loop multiple times 
+
+
+
+// for(let rep = 1; rep <= 10;rep++){
+//     console.log(`lifting weights repetition ${rep}`);
+// }
+
+//while loop
+let rep = 1;
+//define counter outside of loop
+while(rep <= 10){ //condition 
+    console.log(`lifting weights repetition ${rep}`);
+    rep++ //increase it in loop
+}
+//use while loops when you don't need a counter
+let dice = Math.trunc(Math.random() * 6) + 1
+//dice equals a random number between 1 and 6
+while(dice !== 6){
+    //run loop till number is 6 
+    //while number is not 6 keep running
+    console.log(`You rolled a ${dice}`)
+    dice = Math.trunc(Math.random() * 6) + 1
+    //generate a new number each time through till you get a 6
+    if(dice === 6){
+       console.log(`loop is about to end...`) 
+    }
+    //if the number is a six log this statement
+    
+}
+
+
+
