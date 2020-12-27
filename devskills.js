588de9f915a923062a36aedbccb7a5903c91e14b
cep104 <candicelizabethpeters@gmail.com>
2020-12-26 20:40:27 -0600

    refactored

--- devskills.js
+++ devskills.js
@@ -2,15 +2,15 @@ let dataOne = [17, 21, 23];
 let dataTwo = [12, 5, -5, 0, 4];
 
 const printForecast = function (dataArray) {
-  let fullArray = [];
-  for (i = 0; i < dataArray.length; i++) {
+  let fullArray = "";
+  for (let i = 0; i < dataArray.length; i++) {
     if (i === 0) {
-      fullArray.push(`${dataArray[i]}C in ${i + 1} day... `);
+      fullArray = fullArray + `${dataArray[i]}C in ${i + 1} day... `;
     } else {
-      fullArray.push(`${dataArray[i]}C in ${i + 1} days... `);
+      fullArray = fullArray + `${dataArray[i]}C in ${i + 1} days... `;
     }
   }
-  console.log(fullArray.join(""));
+  console.log(fullArray);
 };
 
 printForecast(dataOne);
