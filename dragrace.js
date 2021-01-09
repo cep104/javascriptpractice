e585812e7bd1e7facac6c3f6579930efdb9cfca5
cep104 <candicelizabethpeters@gmail.com>
2021-01-08 22:30:28 -0600

    dragrace

---
+++ dragrace.js
@@ -0,0 +1,39 @@
+const winners = {
+  BebeZaharaBenet: {
+    name: "Bebe Zahara Benet",
+    seasonWon: "S1",
+    memorableQuotes: [
+      "Mother, another day; another slay. Who's first?",
+      "Rrrrra-ka-ta-ti-ti-ta-ta",
+    ],
+    wins: {
+      MaxiChallengeWins: 2,
+      MiniChallengeWins: 0,
+      TimesInTop: 1,
+    },
+    Losses: {
+      TimesPlacedLow: 0,
+      TimesInBottom: 1,
+    },
+  },
+  TyraSanchez: {
+    name: "Tyra Sanchez",
+    seasonWon: "S2",
+    MaxiChallengeWins: 3,
+    MiniChallengeWins: 1,
+    TimesInTop: 4,
+    TimesPlacedLow: 1,
+    TimesInBottom: 0,
+  },
+  Raja: {
+    name: "Raja",
+    seasonWon: "S3",
+    MaxiChallengeWins: 3,
+    MiniChallengeWins: 2,
+    TimesInTop: 4,
+    TimesPlacedLow: 1,
+    TimesInBottom: 1,
+  },
+};
+
+console.log(winners["Raja"]["name"]);
--- objects.js
+++ objects.js
@@ -1,40 +1,53 @@
 const jonas = {
-    firstName: 'Jonas',
-    lastName: 'Schmedtmann',
-    birthYear: 1991,
-    job: 'teacher',
-    friends: ['Michael', 'Peter', 'Steven'],
-    hasDriversLicense: true,
-    // calcAge: function (birthYear){
-    //    return 2037-birthYear;
-    // }
-    // calcAge: function (){
-    //     // console.log(this)
-    //     //returns the whole jonas object
-    //     return 2037-this.birthYear;
-    //  }
-     //this represents jonas in this case
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  birthYear: 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  hasDriversLicense: true,
+  // calcAge: function (birthYear){
+  //    return 2037-birthYear;
+  // }
+  // calcAge: function (){
+  //     // console.log(this)
+  //     //returns the whole jonas object
+  //     return 2037-this.birthYear;
+  //  }
+  //this represents jonas in this case
 
-     calcAge: function() {
-      this.age = 2037 - this.birthYear;
-      return this.age;
-     },
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
 
-     getSummary: function(){
-            return   `${this.firstName} is a ${this.calcAge()}-year old teacher, and ${this.hasDriversLicense ? `has a`:`doesn't have a`} driver's licence`
-         
-     }
-
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old teacher, and ${
+      this.hasDriversLicense ? `has a` : `doesn't have a`
+    } driver's licence`;
+  },
 };
-//any object that is attached to an object is called a method. 
-
+//any object that is attached to an object is called a method.
 
 // console.log(jonas.calcAge(1991));
 // console.log(jonas['calcAge'](1991));
-//access the property using brackets in string then call function with parameter. 
+//access the property using brackets in string then call function with parameter.
 console.log(jonas.calcAge());
 console.log(jonas.age);
-console.log(jonas.getSummary())
+console.log(jonas.getSummary());
+const ul = document.querySelector("ul");
 
-
-
+const countDown = function (startingPoint) {
+  let list = Array.from(Array(startingPoint).keys());
+  for (i = list.length - 1; i >= 0; i--) {
+    if (i === 0) {
+      let li = document.createElement("li");
+      li.innerHTML = "Happy New Year!!";
+      ul.appendChild(li);
+      return "Happy New Year!!";
+    }
+    let li = document.createElement("li");
+    li.innerHTML = list[i];
+    ul.appendChild(li);
+  }
+};
+// countDown(10);
