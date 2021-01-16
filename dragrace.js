07b75c1018905d5d3f6c6ddc73766c201e1548b1
cep104 <candicelizabethpeters@gmail.com>
2021-01-15 23:05:56 -0600

    function added

--- dragrace.js
+++ dragrace.js
@@ -19,25 +19,62 @@ const winners = {
   tyraSanchez: {
     name: "Tyra Sanchez",
     seasonWon: "S2",
-    maxiChallengeWins: 3,
-    miniChallengeWins: 1,
-    timesInTop: 4,
-    timesPlacedLow: 1,
-    timesInBottom: 0,
+    memorableQuotes: [
+      "Mother, another day; another slay. Who's first?",
+      "Rrrrra-ka-ta-ti-ti-ta-ta",
+    ],
+    wins: {
+      maxiChallengeWins: 2,
+      miniChallengeWins: 0,
+      timesInTop: 1,
+    },
+    losses: {
+      timesPlacedLow: 0,
+      timesInBottom: 1,
+    },
   },
-  rajaGemini: {
-    name: "Raja",
-    seasonWon: "S3",
-    maxiChallengeWins: 3,
-    miniChallengeWins: 2,
-    timesInTop: 4,
-    timesPlacedLow: 1,
-    timesInBottom: 1,
-  },
-  sharonNeedles: {
-    name: "Sharon Needles",
-  },
+  // tyraSanchez: {
+  //   name: "Tyra Sanchez",
+  //   seasonWon: "S2",
+  //   maxiChallengeWins: 3,
+  //   miniChallengeWins: 1,
+  //   timesInTop: 4,
+  //   timesPlacedLow: 1,
+  //   timesInBottom: 0,
+  // },
+  // rajaGemini: {
+  //   name: "Raja",
+  //   seasonWon: "S3",
+  //   maxiChallengeWins: 3,
+  //   miniChallengeWins: 2,
+  //   timesInTop: 4,
+  //   timesPlacedLow: 1,
+  //   timesInBottom: 1,
+  // },
+  // // sharonNeedles: {
+  // //   name: "Sharon Needles",
+  // // },
 };
+
+const youreAWinnerBaby = (winners) => {
+  // console.log(winners);
+  let finalScores = [];
+  let winnerInfo = Object.values(winners);
+  for (i = 0; i < winnerInfo.length; i++) {
+    const queen = winnerInfo[i];
+    let queenName = queen["name"];
+    const bigLoss = queen["losses"]["timesInBottom"] * 2;
+    const bigWin = queen["wins"]["maxiChallengeWins"] * 2;
+    const smallWin =
+      queen["wins"]["timesInTop"] + queen["wins"]["miniChallengeWins"];
+    const smallLoss = queen["losses"]["timesPlacedLow"];
+    const totalScore = bigWin + smallWin - (smallLoss + bigLoss);
+
+    finalScores.push({ queenName, totalScore });
+  }
+};
+
+youreAWinnerBaby(winners);
 
 // console.log(winners["Raja"]["name"]);
 // let a = 2;
